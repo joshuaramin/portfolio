55801bda974a313f698dfe420d79fa3d77158704
joshuaramin <raminjoshua05@gmail.com>
2023-01-13 02:37:38 +0800

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 import Layout from '../components/layout/layout'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
